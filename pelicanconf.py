ad31e7199268c2779a691dad1e05a7ac3ccb1448
admiralspark <admiralspark@gmail.com>
2018-03-30 21:08:38 -0800

    Test 1324

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,8 +35,8 @@ DISPLAY_ARTICLE_INFO_ON_INDEX = True
 DISPLAY_RECENT_POSTS_ON_SIDEBAR = True
 NEWEST_FIRST_ARCHIVES = True
 
-BANNER = '/var/www/teamignition.us/content/images/banner.png'
-BANNER_ALL_PAGES = True
+BANNER = 'content/images/banner.png'
+#BANNER_ALL_PAGES = True
 
 # Themes
 THEME = "/home/admiralspark/pelican-themes/pelican-bootstrap3"
